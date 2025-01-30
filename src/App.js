f6cc2dfa9f0831d02fda20a97ead017df16531ea
EbediMeleck <ituzeebedi12@gmail.com>
2025-01-30 05:28:03 -0800

    init message

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+        
+        <nav>
+          <ul className=''>
+            <li className=''>
+              <a >Home</a>
+            </li>
+            <li className=''>
+              <a >Abaut</a>
+            </li>
+            <li className=''>
+              <a >Poject</a>
+            </li>
+            <li className=''>
+              <a >Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
